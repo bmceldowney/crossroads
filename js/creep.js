bc93fd8e3ba1d0607f17989239a19737a17b60a2
Ben McEldowney <bmceldowney@gmail.com>
2014-04-08 19:05:40 -0700

    added findDirection as a prototype method on the creep class for more efficient re-use

--- js/creep.js
+++ js/creep.js
@@ -30,36 +30,35 @@
     XRoads.Creep.prototype.update = function () {
         var x
           , y
-          , step
-          , that = this;
-
-        function findDirection(x, y) {
-            var directions = [{ x: x + 16, y: y }, { y: y, x: x - 16 }, { x: x, y: y + 16 }, { x: x, y: y - 16 }, { x: x, y: y - 16 }]
-              , direction
-              , gridCoords
-              , animations = ['walkRight', 'walkLeft', 'walkDown', 'walkUp', 'walkUp'];
-
-            direction = Math.floor(Math.random() * 4);
-            gridCoords = grid.pointToGrid(directions[direction].x, directions[direction].y);
-            if (grid.isCollision(gridCoords.x, gridCoords.y)) {
-                return findDirection(x, y);
-            }
-
-            that.sprite.animations.play(animations[direction]);
-            return directions[direction];
-        }
+          , step;
 
         if (!this.tween) {
             x = this.sprite.x;
             y = this.sprite.y;
 
-            step = findDirection(x, y);
+            step = this.findDirection(x, y);
 
             this.tween = game.add.tween(this.sprite).to(step, 500, null, true);
             this.tween.onComplete.add(function (tween) {
                 this.tween = null;
             }, this);
         }
+    };
+
+    XRoads.Creep.prototype.findDirection = function (x, y) {
+        var directions = [{ x: x + 16, y: y }, { y: y, x: x - 16 }, { x: x, y: y + 16 }, { x: x, y: y - 16 }, { x: x, y: y - 16 }]
+          , direction
+          , gridCoords
+          , animations = ['walkRight', 'walkLeft', 'walkDown', 'walkUp', 'walkUp'];
+
+        direction = Math.floor(Math.random() * 4);
+        gridCoords = grid.pointToGrid(directions[direction].x, directions[direction].y);
+        if (grid.isCollision(gridCoords.x, gridCoords.y)) {
+            return this.findDirection(x, y);
+        }
+
+        this.sprite.animations.play(animations[direction]);
+        return directions[direction];
     };
 
     XRoads.Creep.prototype.render = function () {
