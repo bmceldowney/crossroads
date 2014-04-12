01c444eb8501cca77d8efefcc25bd5d8e2cea508
Jake Pumpelly <jake.pumpelly@gmail.com>
2014-04-12 00:41:00 -0700

    the movement is divided into 2 steps & tweens.

--- js/creep.js
+++ js/creep.js
@@ -5,7 +5,8 @@
     XRoads.Creep = function (type, x, y) {
         var point = grid.gridToPoint(x, y);
         this.sprite = game.add.sprite(point.x, point.y, type);
-
+        
+        this.moving = false;
         this.sprite.animations.add('walkUp', [0, 1, 2], 6, true);
         this.sprite.animations.add('walkRight', [3, 4, 5], 6, true);
         this.sprite.animations.add('walkDown', [6, 7, 8], 6, true);
@@ -13,37 +14,70 @@
     };
 
     XRoads.Creep.prototype.update = function () {
+        //the movement is divided into 2 steps & tweens.
+        //this is so creeps only occupy a node the instant the sprite cross into it.
+        //Also, it will help us with better looking world wraps.
         var x
           , y
-          , step
-          , shuf;
+          , move
+          //tweens can use relative movement coordinates
+          , step = {x: '+0', y: '+0'};
 
-        if (!this.tween) {
+        if (!this.moving) {
             x = this.sprite.x;
             y = this.sprite.y;
+            this.moving = true;
+            move = this.findDirection(x, y);
 
-            step = this.findDirection(x, y);
-            //shuf = { x: 0, y: 0 };
-            //shuf.x = (step.x - x) * .5 + x;
-            //shuf.y = (step.y - y) * .5 + y;
-
-            if (Math.abs(step.x - x) < 100 && Math.abs(step.y - y) < 100) {
-                this.tween = game.add.tween(this.sprite).to(step, 500, null, true);
-            } else {
-                //World wrap occurs
-                if (Math.abs(step.x - x)) {
-                    this.sprite.x = step.x;
-                } else {
-                    this.sprite.y = step.y;
-                }
-                this.tween = game.add.tween(this.sprite).to(step, 500, null, true);
+            switch (move.letter) {
+                case 'n' :
+                    step.y = '+8';
+                    break;
+                case 's' :
+                    step.y = '-8';
+                    break;
+                case 'e' :
+                    step.x = '+8';
+                    break;
+                case 'w' :
+                    step.x = '-8';
+                    break;
+                default :
+                    step.x = '+0';
+                    step.y = '+0';
+                    break;
             }
+
+            this.tween1 = game.add.tween(this.sprite).to(step, 500, null, true);
+            this.tween1.onComplete.add(onStepComplete, this);
+
+            function onStepComplete() {
+                this.tween2 = game.add.tween(this.sprite).to(step, 4500, null, true);
+                this.tween2.onComplete.add(onDoneComplete, this);
+            };
+            function onDoneComplete() {
+                this.moving = false;
+            };
+            //if (Math.abs(step.x - x) < 100 && Math.abs(step.y - y) < 100) {
+            //    this.tween1 = game.add.tween(this.sprite).to(shuf, 500, null, true);
+            //} else {
+            //    //World wrap occurs
+            //    if (Math.abs(step.x - x)) {
+            //        this.sprite.x = step.x;
+            //    } else {
+            //        this.sprite.y = step.y;
+            //    }
+            //    this.tween = game.add.tween(this.sprite).to(step, 500, null, true);
+            //}
+
             
-            this.tween.onComplete.add(function (tween) {
-                this.tween = null;
-            }, this);
+
+            //this.tween.onComplete.add(function (tween) {
+            //    this.tween = null;
+            //}, this);
         }
     };
+    
 
     XRoads.Creep.prototype.findDirection = function (x, y) {
         var gridCoords
