48ae6f611fe22faf7c52ad4cc3c6c8de72e589b0
Ben McEldowney <bmceldowney@gmail.com>
2014-10-18 09:04:39 -0700

    converting combat.js

--- game/states/combat.js
+++ game/states/combat.js
@@ -1,26 +1,42 @@
 'use strict';
-  function Combat() {}
-  Combat.prototype = {
-    preload: function() {
-      // Override this method to add some load operations. 
-      // If you need to use the loader, you may need to use them here.
-    },
-    create: function() {
-      // This method is called after the game engine successfully switches states. 
-      // Feel free to add any setup code here (do not load anything here, override preload() instead).
-    },
-    update: function() {
-      // state update code
-    },
-    paused: function() {
-      // This method will be called when game paused.
-    },
-    render: function() {
-      // Put render operations here.
-    },
-    shutdown: function() {
-      // This method will be called when the state is shut down 
-      // (i.e. you switch to another state from this one).
+function Combat() {}
+Combat.prototype = {
+  preload: function () {
+    XRoads.Map.preload(this.game);
+    XRoads.CM = this.creeps = new XRoads.CreepManager(this.game);
+    XRoads.CombatPlayer = new CombatPlayer(this.game);
+    XRoads.CombatPlayer.preload();
+    XRoads.Behaviors = new XRoads.Behaviors(this.game);
+    XRoads.Behaviors.preload();
+  },
+
+  create: function () {
+    XRoads.Map.create();
+    XRoads.Grid.create(this.game);
+    XRoads.GridNodes.create(XRoads.Grid.getColumns(), XRoads.Grid.getRows(), 16);
+    this.creeps.populate();
+    XRoads.CombatPlayer.create();
+    XRoads.Behaviors.create();
+    this.game.stage.smoothed = false;
+    this.game.stage.backgroundColor = '#000000';
+    // Hax0rz for IE
+    if (this.game.context.msImageSmoothingEnabled) {
+        this.game.context.msImageSmoothingEnabled = false;
     }
-  };
+  },
+
+  update: function () {
+    XRoads.CombatPlayer.update();
+  },
+  paused: function() {
+    // This method will be called when game paused.
+  },
+  render: function() {
+    // Put render operations here.
+  },
+  shutdown: function() {
+    // This method will be called when the state is shut down 
+    // (i.e. you switch to another state from this one).
+  }
+};
 module.exports = Combat;
